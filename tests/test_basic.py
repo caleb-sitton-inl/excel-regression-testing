1dde46f7f699f3ea601e4e249add7dc114b21b57
Caleb Sitton <caleb.sitton@inl.gov>
2025-11-14 14:34:10 -0600

    Added simple workbook containing macros with test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -4,16 +4,20 @@ import os
 import xlwings as xw
 
 tests_dir_path = os.path.dirname(__file__)
-test_xl_path = os.path.join(os.path.dirname(tests_dir_path), 'workbooks', 'sample_workbook.xlsx')
+test_xl_path = os.path.join(os.path.dirname(tests_dir_path), 'workbooks', 'basic_workbook.xlsx')
 
-def test_sample():
+def test_basic():
   with xw.Book(test_xl_path) as workbook:
     sheet = workbook.sheets[0]
 
     # Set input
     input_cell = sheet["A2"]
-    input_cell.value = 1.0
+    input_cell.value = 2.0
 
     # Check output
     output_cell = sheet["B2"]
-    assert output_cell.value == 5.0
+    assert output_cell.value == 4.0
+
+
+if __name__ == "__main__":
+  test_basic()
---
+++ tests/test_macros.py
@@ -0,0 +1,26 @@
+"""Tests simple macros in an excel workbook"""
+import os
+
+import xlwings as xw
+
+tests_dir_path = os.path.dirname(__file__)
+test_xl_path = os.path.join(os.path.dirname(tests_dir_path), 'workbooks', 'with_macros.xlsm')
+
+def test_macros():
+  with xw.Book(test_xl_path) as workbook:
+    sample_macro = workbook.macro("sample_macro")
+    sample_macro()
+
+    sheet = workbook.sheets[0]
+    output_cell = sheet["B2"]
+    assert output_cell.value == 2.0
+
+    undo_sample = workbook.macro("undo_sample")
+    undo_sample()
+
+    assert output_cell.value == 0.0
+    assert output_cell.formula != ''
+
+
+if __name__ == "__main__":
+  test_macros()
